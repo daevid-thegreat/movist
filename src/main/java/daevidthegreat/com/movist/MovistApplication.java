94d8bf8677411cfd6a340204a967ef1a2232dc31
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-12-28 19:30:57 +0100

    first commit

---
+++ src/main/java/daevidthegreat/com/movist/MovistApplication.java
@@ -0,0 +1,13 @@
+package daevidthegreat.com.movist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daevidthegreat/com/movist/MovistApplicationTests.java
@@ -0,0 +1,13 @@
+package daevidthegreat.com.movist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
