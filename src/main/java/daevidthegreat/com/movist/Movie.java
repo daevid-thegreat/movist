8cd9f43287d9e522ba7229f4a23cc02552ebf4da
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-12-29 14:26:59 +0100

    Update on Movie class

--- src/main/java/daevidthegreat/com/movist/Movie.java
+++ src/main/java/daevidthegreat/com/movist/Movie.java
@@ -1,4 +1,25 @@
 package daevidthegreat.com.movist;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
+    private String id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String posterPath;
+    private List<String> genres;
+    private List<String> backdrops;
 }
